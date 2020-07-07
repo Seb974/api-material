cbb2be7c7eb407c2e2edad3ce47150af22eb6924
Seb97_4 <m_seb@icloud.com>
2020-07-07 20:45:10 +0400

    deployment test

---
+++ api/src/Entity/Booking.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=BookingRepository::class)
+ */
+class Booking
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=60)
+     */
+    private $name;
+
+    // @Assert\DateTime
+    /**
+     * 
+     * @ORM\Column(type="datetime")
+     */
+    private $checkIn;
+
+    /**
+     * 
+     * @ORM\Column(type="datetime")
+     */
+    private $checkOut;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCheckIn(): ?\DateTimeInterface
+    {
+        return $this->checkIn;
+    }
+
+    public function setCheckIn(\DateTimeInterface $checkIn): self
+    {
+        $this->checkIn = $checkIn;
+
+        return $this;
+    }
+
+    public function getCheckOut(): ?\DateTimeInterface
+    {
+        return $this->checkOut;
+    }
+
+    public function setCheckOut(\DateTimeInterface $checkOut): self
+    {
+        $this->checkOut = $checkOut;
+
+        return $this;
+    }
+}
---
+++ api/src/Entity/Message.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass=MessageRepository::class)
+ */
+class Message
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(type="string", length=60)
+     */
+    private $name;
+
+    /**
+     * @Assert\Email
+     * @ORM\Column(type="string", length=60)
+     */
+    private $email;
+
+    /**
+     * @Assert\NotBlank
+     * @ORM\Column(type="text")
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
